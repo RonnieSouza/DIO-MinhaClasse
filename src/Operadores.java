f5f5df50921787b2374e1fb25c3d545edbdb0b23
Ronnie <ronnievicente.souza@hotmail.com>
2023-03-07 21:52:26 -0300

    incrementando operadores java

--- src/Operadores.java
+++ src/Operadores.java
@@ -1,5 +1,22 @@
 public class Operadores {
     public static void main(String[] args) {
+        String contatenacao = "?";
+
+        contatenacao = 1+1+1+1+"1";
+
+        System.out.println(contatenacao);
+
+        contatenacao = 1+"1"+1+1;
+
+        System.out.println(contatenacao);
+
+        contatenacao = 1+"1"+1+"1";
+
+        System.out.println(contatenacao);
+
+        contatenacao = "1"+(1+1+1);
+
+        System.out.println(contatenacao);
 
     }
 }
